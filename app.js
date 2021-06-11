7982b87fe2e0a5e77a5d2d88598a846704fea56b
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-06-11 12:14:02 +0530

    Update app.js
    added the new yargs commands
    add
    remove
    read
    list

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log(process.argv); 
+const yargs = require("yargs");
+
+// we can specify the version using args
+yargs.version("1.0.23");
+
+// add command
+
+yargs.command({
+  command: "add",
+  describe: "this will add notes to the appllication",
+  handler: () => console.log("adding notes"),
+});
+
+// remove command
+
+yargs.command({
+  command: "remove",
+  describe: "removing notes from the application",
+  handler: () => console.log("removing notes"),
+});
+
+// read notes
+
+yargs.command({
+  command: "read",
+  describe: "reading a note from the application",
+  handler: () => console.log("reading a note"),
+});
+
+// list notes
+
+yargs.command({
+  command: "list",
+  describe: "listing all the notes from the application",
+  handler: () => console.log("listong all the notes"),
+});
+
+console.log(yargs.argv);
